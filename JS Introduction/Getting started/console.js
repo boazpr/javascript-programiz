d913abd0bed1c345d66c87c5eabfcb3361603507
boazpr <boazprathaban@gmail.com>
2025-03-27 11:23:56 +0530

    3rd commit

--- JS Introduction/Getting started/console.js
+++ JS Introduction/Getting started/console.js
@@ -1,2 +1,6 @@
 let message = "Hello, JavaScript!";
 console.log(message)
+console.log("Good Morning!");
+console.log(2000);
+const a = "Hello";
+console.log(a);
---
+++ JS Introduction/Getting started/datatype.js
@@ -0,0 +1,28 @@
+let fruit = 'apple';
+console.log(fruit)
+let integer_number = -3;
+console.log(integer_number);
+let float_number = 3.15;
+console.log(float_number);
+// BigInt value
+let value1 = 900719925124740998n;
+
+// add two big integers
+let result1 = value1 + 1n;
+console.log(result1);  // "900719925124740999n"
+
+let value2 = 900719925124740998n;
+let dataChecked = true;
+console.log(dataChecked);
+let name = undefined;
+console.log(name); 
+let number = null;
+console.log(number);
+let x = Symbol("boaz");
+let y = Symbol("boaz");
+console.log(x == y);
+let student = {
+    firstName: "John",
+    lastName: null,
+    class: 10};
+console.log(student);
