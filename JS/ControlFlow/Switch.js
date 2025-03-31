2d81d99b5022e5a3580a49ae525d591bb2814a5c
boazpr <boazprathaban@gmail.com>
2025-03-31 09:52:19 +0530

    12th

---
+++ JS/ControlFlow/Switch.js
@@ -0,0 +1,57 @@
+let trafficLight = "green";
+let message = ""
+ 
+switch (trafficLight) {
+    case "red":
+        message = "Stop immediately.";
+        break;
+    case "yellow":
+        message = "Prepare to stop.";
+        break;
+    case "green":
+        message = "Proceed or continue driving.";
+        break;
+    default:
+        message = "Invalid traffic light color.";
+}
+ 
+console.log(message)
+ 
+// Output: Proceed or continue driving.
+
+let day = 3; 
+let activity;
+
+switch (day) {
+
+    case 1:
+        console.log("Sunday");
+        break;
+
+    case 2:
+        console.log("Monday");
+        break;
+
+    case 3:
+        console.log("Tuesday");
+        break;
+
+    case 4:
+        console.log("Wednesday");
+        break;
+
+    case 5:
+        console.log("Thursday");
+        break;
+
+    case 6:
+        console.log("Friday");
+        break;
+
+    case 7:
+        console.log("Saturday");
+        break;
+
+    default:
+        console.log("Invalid Day");
+}
