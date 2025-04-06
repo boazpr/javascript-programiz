24b2a543986bb7fd203ddb2e9e26d224a3400439
boazpr <boazprathaban@gmail.com>
2025-04-06 18:11:54 +0530

    17th

---
+++ JS/Exception&Modules/try_catch_finally.js
@@ -0,0 +1,17 @@
+// program to show try...catch in a program
+
+const numerator= 100, denominator = 'a';
+
+try {
+     console.log(numerator/denominator);
+
+    // forgot to define variable a      
+    console.log(a);
+}
+catch(error) {
+    console.log('An error caught'); 
+    console.log('Error message: ' + error);  
+}
+finally {
+    console.log('Finally will execute every time');
+}
--- JS/Types/Symbols.js
+++ JS/Types/Symbols.js
@@ -14,3 +14,15 @@ let person = {
 
 console.log(person); // {name: "Jack", Symbol(id): 123}
 console.log(person[id]); // 123
+
+let person11 = {
+    name: "Jack"
+};
+
+// using string as key
+person.id = 12;
+console.log(person.id); // 12
+
+// Another program overwrites value
+person11.id = 'Another value';
+console.log(person.id); // Another value
